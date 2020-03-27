907880dfb9cad9b7cdf4afd0489e4529bd6f4196
Trent Rolf <trentrolf@gmail.com>
2020-03-27 17:32:29 -0600

    Initializes correctly now. Accidentally using debug pins for UART.

--- src/custom_board.h
+++ src/custom_board.h
@@ -1,8 +1,11 @@
 #ifndef CUSTOM_BOARD_H
 #define CUSTOM_BOARD_H
 
-#define RX_PIN_NUMBER 1
-#define TX_PIN_NUMBER 0
+#define EXT_DIG1 2 /* On the USB Port P2 (D-), and P3 of the spring connector */
+#define EXT_DIG2 3 /* On the USB Port P3 (D+), and P4 of the spring connector */
+
+#define RX_PIN_NUMBER EXT_DIG2 // NRF UART peripherial's RX
+#define TX_PIN_NUMBER EXT_DIG1 // NRF UART peripherial's TX
 #define CTS_PIN_NUMBER 0xFFFFFFFF // NRF_UARTE_PSEL_DISCONNECTED
 #define RTS_PIN_NUMBER 0xFFFFFFFF // NRF_UARTE_PSEL_DISCONNECTED
 
